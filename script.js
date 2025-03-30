faed63f8a38e05e40146ad2c65ed7cd796a1fa62
Utkarsh Sonawane <80320999+TekCrec@users.noreply.github.com>
2025-03-30 19:15:27 +0400

    Updated website

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const main = document.querySelector("main");
+    main.style.opacity = "0";
+    main.style.transition = "opacity 1s ease-in-out";
+    setTimeout(() => {
+        main.style.opacity = "1";
+    }, 300);
+});
+
 console.log("JavaScript is working!");
